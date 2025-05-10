5d9a01614d68eb0e39174b60bf5d4f59f6c50f6a
Dima <d.chernushin@gmail.com>
2025-05-10 19:36:36 +0300

    Carousel flask v2

--- flask_carousel/carousel.py
+++ flask_carousel/carousel.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from itertools import count
+
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +8,23 @@ app = Flask(__name__)
 @app.route('/carousel')
 def carousel():
     return render_template('carousel.html')
+
+
+default_images = ['mars1.webp', 'mars2.jpg', 'mars3.jpg']
+
+
+@app.route('/galery', methods=['POST', 'GET'])
+def gallery():
+    if request.method == 'POST':
+        file = request.files.get('new_image')
+        if file is not None:
+            print(file.content_type)
+            name = f'img_{next(count())}.jpg'
+            full_name = url_for('static', filename=f'img/{name}')[1:]
+            with open(full_name, 'wb') as target_file:
+                target_file.write(file.read())
+                default_images.append(name)
+    return render_template('gallery.html', images=default_images)
 
 
 if __name__ == '__main__':
